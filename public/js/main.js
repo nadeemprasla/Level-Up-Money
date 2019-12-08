1173bf38f8e96dd41a832aa031c5a36759b851d4
Nadeem Prasla <nadeemprasla@yahoo.com>
2019-12-08 14:46:08 -0600

    added chartJS and configuration for charts to a modal

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
 
     console.log(userId, "id")
 
-    $(document).on("click",".categorySbtn", handleSubmit);
+    $(document).on("click", ".categorySbtn", handleSubmit);
 
-    $(document).on("click","#addCat", function (e) {
+    $(document).on("click", "#addCat", function (e) {
         e.preventDefault();
         if (validator()) {
             $("#categoryModel").modal("toggle");
@@ -30,12 +30,12 @@ $(document).ready(function () {
         // userId = categoryInput.attr("data-userid")
         newCategory = $("#categoryName").val();
         var month_name = $("#monthChoice").val();
-        
-        submitPost(userId, newCategory,month_name)
+
+        submitPost(userId, newCategory, month_name)
     }
 
-    function submitPost(userId, newCategory,month_name) {
-        
+    function submitPost(userId, newCategory, month_name) {
+
         $.post("/api/category/posts", {
             category_name: newCategory,
             UserId: userId,
@@ -54,7 +54,7 @@ $(document).ready(function () {
 
         e.preventDefault();
         e.stopPropagation();
-        console.log(e.which); 
+        console.log(e.which);
         if (validator()) {
             categoryid = e.target.dataset.catid;
             $("#enteriesModal").modal("toggle");
@@ -66,7 +66,7 @@ $(document).ready(function () {
     })
 
 
-    $(document).on("click","#submit", function (e) {
+    $(document).on("click", "#submit", function (e) {
         e.preventDefault();
         var name = $("#entryName").val();
         var amount = $("#amount").val();
@@ -81,9 +81,9 @@ $(document).ready(function () {
             memo: memo,
             CategoryId: currentcat,
             UserId: userId,
-            AllowanceId:month
+            AllowanceId: month
         }
-        
+
         $.post("/api/addItem", data).then(function (result) {
 
             $("#enteriesModal").modal("hide");
@@ -106,37 +106,37 @@ $(document).ready(function () {
     var newBudget;
 
     $(document).on("keyup", "#budget", function (e) {
-        
+
         e.stopPropagation();
         e.preventDefault();
-        
+
         var data = {};
         var total_budget = $(this).val();
         var month_name = $("#monthChoice").val();
-        
+
         if (e.which === 13) {
             data = {
                 total_budget: total_budget,
                 month_name: month_name,
                 UserId: userId
             }
-            newBudget =total_budget;
-            
+            newBudget = total_budget;
+
             updateBudget(data);
         };
     });
-    
-    $(document).on("click","#budget",function(e){
-       
+
+    $(document).on("click", "#budget", function (e) {
+
         var total_budget = $(this).val();
         $("#budget").val(total_budget);
-        
+
     });
 
     $(document).on("keyup", "#othrIncome", function (e) {
         e.stopPropagation();
         e.preventDefault();
-        
+
         var data = {};
         var extra_income = $(this).val();
         var total_budget = currentBudget
@@ -170,28 +170,28 @@ $(document).ready(function () {
     });
 
     function updateBudget(data) {
-        
+
         $.ajax({
             method: "PUT",
             url: "/api/budget",
             data: data
         }).then(function (result) {
-            
-        
+
+
         });
 
     }
 
     // Month change
     //----------------------------------------------------
-    $(document).on("change",'#monthChoice',function() {
+    $(document).on("change", '#monthChoice', function () {
         var month = $("#monthChoice option:selected").val();
         var id = userId;
-        $.get("/api/"+id+"/"+month,function(result){
-            
-            document.documentElement.innerHTML=result;
+        $.get("/api/" + id + "/" + month, function (result) {
+
+            document.documentElement.innerHTML = result;
             $("#monthChoice").val(month);
-            
+
         })
 
     });
@@ -200,22 +200,22 @@ $(document).ready(function () {
     //----------------------------------------
 
     $(document).on("keyup", "#plannedAmt", function (e) {
-        
+
         e.stopPropagation();
         e.preventDefault();
 
-        var categoryId=$(this).parent().parent().attr('id');
-        
+        var categoryId = $(this).parent().parent().attr('id');
+
         var data = {};
         var category_amount = $(this).val();
-        
+
         var month_name = $("#monthChoice").val();
         if (e.which === 13) {
             data = {
                 category_amount: category_amount,
                 month_name: month_name,
                 UserId: userId,
-                id:categoryId
+                id: categoryId
             }
             categoryAmt = category_amount;
             updateCategory(data);
@@ -223,19 +223,91 @@ $(document).ready(function () {
     });
 
     function updateCategory(data) {
-        
+
         $.ajax({
             method: "PUT",
             url: "/api/categoryAmt",
             data: data
         }).then(function (result) {
-            
+
         });
 
     }
-    
+
+
+
+
+
+    // Chart
+    // ----------------------------------------------------------
+    var chartType = "doughnut";
+
+    $("#expenseChart").on("click", () => {
+        $("#chartModal").modal("toggle");
+        chartType = "doughnut"
+        var monthChoice = $("#monthChoice").val();
+        runChart(userId, monthChoice);
+    })
+
+    $(document).on("click","#btnChartType", (e) => {
+        chartType = e.target.value
+    console.log(chartType)
+        $("#chartcontainer").html("")
+        var monthChoice = $("#monthChoice").val();
+        runChart(userId, monthChoice);
+    })
+
+    function runChart(userId, monthChoice) {
+        $.get("/chart/" + userId + "/" + monthChoice, (e) => {
+            console.log(e)
+            chartLabel = e.chartLabel
+            chartName = e.chartName
+            chartLabel.map((e) => {
+                console.log(e)
+                console.log(chartName[e])
+                createChart(e, chartType, chartName[e], chartName[e + "Price"])
+            });
+
+        })
+    }
+
+
+    function createChart(id, type, labels, data) {
+        $("#chartcontainer").append(`<div class="row chartrow"><div class="col colchart"><canvas id="${id}" class="myCharts"></canvas></div></div>`);
+        var ctx = document.getElementById(id).getContext('2d');
+        
+        var chart = new Chart(ctx, {
+            type: type,
+
+            data: {
+                labels: labels,
+                datasets: [{
+                    backgroundColor: ['rgb(255, 99, 132)','rgb(0, 0, 255)','RGB(181,114,129)','RGB(118,255,122)','RGB(200,8,21)','RGB(48,213,200)','RGB(255,29,206)','RGB(255,248,220)','RGB(64,130,109)' ],
+                    borderColor: 'rgb(255, 199, 132)',
+                    data: data,
+
+                }]
+            },
+
+            options: {
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0
+                    }
+                }
+            }
+        })
+    }
+
+
+
+
+
+    // -----------------------------------------------------------
+    // Chart Ends
 
 
 });
-
-
--- server/controllers/get-api-routes.js
+++ server/controllers/get-api-routes.js
@@ -1,116 +1,166 @@
 var db = require("../models");
-var allowance ;
+var allowance;
 var userInfo;
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api", function(req, res) {
-    db.User.findAll({}).then(function(data) {
-      user = data.username
-      res.json(data);
+    app.get("/api", function (req, res) {
+        db.User.findAll({}).then(function (data) {
+            user = data.username
+            res.json(data);
+        });
     });
-  });
-  
-  app.get("/api/:id/:month",function(req,res){
-    userInfo = req.user;
-    var month = req.params.month
-    var userId = req.params.id
-    db.Allowance.findOne({
-        where: {
-            $and:{
-                month_name:month,
-                UserId:userId
-              }
-           
-        },
-        raw: true,
-    }).then((data) => {
-        
-          allowance = data
-          getCategories(userId,month,res)
 
-    })
-  })
+    app.get("/api/:id/:month", function (req, res) {
+        userInfo = req.user;
+        var month = req.params.month
+        var userId = req.params.id
+        db.Allowance.findOne({
+            where: {
+                $and: {
+                    month_name: month,
+                    UserId: userId
+                }
 
-  function getCategories(userId,month,res){
-  
-    db.Category.findAll({
-        where: {
-            $and: {
-                AllowanceId: month,
-                UserId: userId
-            }
-        },
-        raw: true,
-        include: [db.Entries]
-    }).then((data) => {
-        
-        allData = [];
-        data.map((e) => {
-            catUpperCase = e.category_name.toLowerCase()
-            catUpperCase = catUpperCase.charAt(0).toUpperCase() + catUpperCase.slice(1)
-            if (e["Entries.entry_name"]) {
+            },
+            raw: true,
+        }).then((data) => {
 
-                entryUpperCase = e["Entries.entry_name"];
-                entryUpperCase = entryUpperCase.toLowerCase();
-                entryUpperCase = entryUpperCase.charAt(0).toUpperCase() + entryUpperCase.slice(1)
-                allData.push({
-                    "category_id": e.id,
-                    "category_name": catUpperCase,
-                    "category_amount":e.category_amount,
-                    "entry_id": e["Entries.id"],
-                    "entry_name": entryUpperCase,
-                    "amount": e["Entries.amount"],
-                    "memo": e["Entries.memo"],
-                    "amountType": e["Entries.amountType"]
-                })
-            }
-           
+            allowance = data
+            getCategories(userId, month, res)
+
         })
-        
-        getEnteries(userId,month,res)
+    })
+
+    function getCategories(userId, month, res) {
+
+        db.Category.findAll({
+            where: {
+                $and: {
+                    AllowanceId: month,
+                    UserId: userId
+                }
+            },
+            raw: true,
+            include: [db.Entries]
+        }).then((data) => {
+
+            allData = [];
+            data.map((e) => {
+                catUpperCase = e.category_name.toLowerCase()
+                catUpperCase = catUpperCase.charAt(0).toUpperCase() + catUpperCase.slice(1)
+                if (e["Entries.entry_name"]) {
+
+                    entryUpperCase = e["Entries.entry_name"];
+                    entryUpperCase = entryUpperCase.toLowerCase();
+                    entryUpperCase = entryUpperCase.charAt(0).toUpperCase() + entryUpperCase.slice(1)
+                    allData.push({
+                        "category_id": e.id,
+                        "category_name": catUpperCase,
+                        "category_amount": e.category_amount,
+                        "entry_id": e["Entries.id"],
+                        "entry_name": entryUpperCase,
+                        "amount": e["Entries.amount"],
+                        "memo": e["Entries.memo"],
+                        "amountType": e["Entries.amountType"]
+                    })
+                }
+
+            })
+
+            getEnteries(userId, month, res)
+
+        }
+        )
+    }
+
+    function getEnteries(user, month, res) {
+
+        db.Category.findAll({
+            where: {
+
+                $and: {
+                    AllowanceId: month,
+                    UserId: user
+                }
+
+            },
+            raw: true
+        }).then((data) => {
+            category = [];
+            data.map((e) => {
+                upperit = e.category_name.toLowerCase()
+                upperit = upperit.charAt(0).toUpperCase() + upperit.slice(1)
+                category.push({
+                    "id": e.id,
+                    "category_name": upperit,
+                    "category_amount": e.category_amount,
+                })
+            })
+
+            res.render('home', {
+                user: userInfo,
+                alldata: allData,
+                category: category,
+                allowance: allowance,
+                currentMonth: month
+            })
+
+        })
 
     }
+
+    app.get("/home", function (req, res) {
+        res.render('home', { user: "" });
+    })
+
+
+    app.get('/chart/:id/:month', function (req, res) {
+        console.log("reqbody        ", req.params.id, req.params.month);
+        db.Category.findAll({
+            where: {
+                $and: {
+                    AllowanceId: req.params.month,
+                    UserId: req.params.id
+                }
+            },
+            raw: true,
+            include: [db.Entries]
+        }).then((data) => {
+            chartLabel = [];
+            chartName= {};
+            chartData={};
+            data.map((e) => {
+                console.log("entry",e)
+                console.log(e.id)
+                console.log(e['Entries.entry_name'])
+                if(chartLabel.includes(e.category_name)) {
+                    console.log("its there")
+                }
+                else{
+                    // catUpperCase = e.category_name.toLowerCase()
+                    // catUpperCase = catUpperCase.charAt(0).toUpperCase() + catUpperCase.slice(1)    
+                    chartLabel.push(e.category_name);
+                    console.log("added")}
+                if(chartName.hasOwnProperty(e.category_name)) {
+                    chartName[e.category_name].push(e['Entries.entry_name'])
+                    chartName[e.category_name + "Price"].push(e['Entries.amount'])
+                }
+                else{
+                    chartName[e.category_name] = [e['Entries.entry_name']]
+                    chartName[e.category_name + "Price"] = [e['Entries.amount']]
+                }
+                // if(chartData.hasOwnProperty(e['Entries.entry_name'])) {
+                //     chartData[e['Entries.entry_name']] = e['Entries.amount']
+                // }
+                // else{
+                //     chartData[e['Entries.entry_name']] = e['Entries.amount']
+                // }
+            })
+            console.log(chartLabel)
+            console.log(chartName);
+            // console.log(chartData);
+            res.json({chartLabel,chartName})
+        })
+    }
     )
-  }
-
-  function getEnteries(user,month,res){
-
-    db.Category.findAll({
-      where: {
-
-          $and:{
-              AllowanceId:month,
-              UserId:user
-          }
-          
-      },
-      raw: true
-    }).then((data) => {
-      category = [];
-      data.map((e) => {
-          upperit = e.category_name.toLowerCase()
-          upperit = upperit.charAt(0).toUpperCase() + upperit.slice(1)
-          category.push({
-              "id": e.id,
-              "category_name": upperit,
-              "category_amount":e.category_amount,
-          })
-      })
-
-      res.render('home', {
-          user: userInfo,
-          alldata: allData,
-          category: category,
-          allowance: allowance,
-          currentMonth: month
-      })
-
-  })
-
-  }
-
-  app.get("/home",function(req,res){
-    res.render('home',{user:""});
-  })
-
 }
